8f328cba1d2240f07134c1cca39b72351c21c06f
ashankdavid <ashank2507david@gmail.com>
2026-03-13 16:37:38 +0530

    Today was Day 11 we completed LinkedList and started Stacks

--- src/DataStructures/LinkedList/Singly/SLL.java
+++ src/DataStructures/LinkedList/Singly/SLL.java
@@ -36,8 +36,67 @@ public class SLL {
         temp.next = n;
     }
 
+    int getSize(){
+        int size = 0;
+        Node temp = head;
+        while(temp!=null){
+            temp = temp.next;
+            size++;
+        }
+        return size;
+    }
+
     void insertAtPos(int val, int pos){
-        // Complete this code...
+        int size = getSize();
+        Node n = new Node(val);
+        if(pos<0 || pos>size){
+            System.out.println("Invalid Position Given!");
+        }else if(pos==0){
+            n.next = head;
+            head = n;
+        }else{
+            Node temp = head;
+            while(--pos>0){
+                temp = temp.next;
+            }
+            n.next = temp.next;
+            temp.next = n;
+        }
+
+    }
+
+    void deleteAtHead(){
+        if(head==null){
+            System.out.println("Get Lost!");
+            return;
+        }
+        Node toDelete = head;
+        head = head.next;
+        toDelete = null;
+    }
+
+    void deleteByValue(int val){
+        if(head==null){return;}
+
+        if(head.data==val){
+            deleteAtHead();
+            return;
+        }
+
+        Node temp = head;
+        while(temp.next!=null && temp.next.data!=val){
+            temp = temp.next;
+        }
+
+        if(temp.next == null){
+            System.out.println("Value is not found");
+            return ;
+        }
+
+        Node toDelete = temp.next;
+        temp.next = temp.next.next;
+        toDelete = null;
+
     }
 
     void display(){
@@ -61,6 +120,16 @@ class Driver{
         sll.insertAtHead(40);
         sll.insertAtHead(50);
         sll.insertAtHead(60);
+        sll.display();
+        sll.insertAtPos(5000, 2);
+        sll.display();
+        sll.deleteAtHead();
+        sll.display();
+        sll.deleteByValue(5000);
+        sll.display();
+        sll.deleteByValue(500);
+        sll.display();
+        sll.deleteByValue(50);
         sll.display();
     }
 }
---
+++ src/DataStructures/Stack/Stack.java
@@ -0,0 +1,68 @@
+package DataStructures.Stack;
+
+public class Stack {
+    int[] arr;
+    int top;
+    int size;
+
+    Stack(int size){
+        this.size = size;
+        top = -1;
+        arr = new int[size];
+    }
+
+    void push(int val){
+        if(top==size-1){
+            System.out.println("Stack Overflown!");
+            return ;
+        }
+        top++;
+        arr[top] = val;
+    }
+
+    void pop(){
+        if(top==-1){
+            System.out.println("Stack Underflown!");
+            return ;
+        }
+        arr[top] = 0; // optional
+        top--;
+    }
+
+    int Top(){
+        if(top==-1){
+            System.out.println("Stack Underflown!");
+            return -1;
+        }
+        return arr[top];
+    }
+
+    boolean isEmpty(){
+        return top == -1;
+    }
+}
+
+class Driver{
+    public static void main(String[] args) {
+        Stack st = new Stack(5);
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+
+        System.out.println(st.Top());
+
+        st.pop();
+        System.out.println(st.Top());
+        st.pop();
+        System.out.println(st.Top());
+        st.pop();
+        System.out.println(st.Top());
+        st.pop();
+        System.out.println(st.Top());
+        st.pop();
+        System.out.println(st.Top());
+        System.out.println(st.isEmpty());
+    }
+}
